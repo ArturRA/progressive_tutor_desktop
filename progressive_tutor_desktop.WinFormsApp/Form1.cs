784348b945352a28b62d13e70627baa692bc995f
Artur Ribeiro Alfa <phoenixrebirth1324@gmail.com>
2023-06-19 16:51:56 -0300

    Add project files.

---
+++ progressive_tutor_desktop.WinFormsApp/Form1.cs
@@ -0,0 +1,10 @@
+namespace progressive_tutor_desktop.WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
